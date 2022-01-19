7f6e4d360c92cda01e1c0a23b39bdb60bf1b9325
ZarbaliyevOzal <34451434+ZarbaliyevOzal@users.noreply.github.com>
2022-01-19 14:10:18 +0400

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/test', (req, res) => {
+  res.send('test route')
+})
+
+app.listen(process.env.APP_PORT, () => {
+  console.log(`Example app listening at http://localhost:${process.env.APP_PORT}`)
+})
